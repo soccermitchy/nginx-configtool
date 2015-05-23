ae2d44e1975180c5b4ebba7d4399ea486abdd3c5
Mitchell Monahan <soccermitchy@gmail.com>
2015-05-22 23:03:00 -0400

    Fix documentation

--- menu.lua
+++ menu.lua
@@ -1,35 +1,43 @@
---- @author Mitchell Monahan
+------------------------------- 
+-- @author Mitchell Monahan
 -- @copyright 2015 Mitchell Monahan
 local class = require'middleclass'
 local Menu = class("menu")
 
---- Called when a new menu is created
--- Defaults to no items
--- Defaults to a title of 'Undefined'
+-------------------------------
+-- Called when a new menu is created.
+-- Defaults to no items.
+-- Defaults to a title of 'Undefined'.
 -- Called automatically when you do Menu:new()
 function Menu:initialize()
 	self.items={}
 	self.title = 'Undefined'
 end
 
---- Set the title for the menu
+-------------------------------
+-- Sets title for the menu
 -- @param title The name of the menu
 -- @usage menu:setTitle("Test Menu")
 function Menu:setTitle(title)
 	self.title=title
 end
 
---- Add an item to the menu
+-------------------------------
+-- Add an item to the menu
 -- @param name The name of the item, as displayed to the user
 -- @param callback_func A function that will be called when the user selects the item
--- @usage menu:addItem("Select Me!",function() print'hi' end
+-- @return Number of menu items, could be used as an item ID
+-- @usage menu:addItem("Select Me!",function() print'hi' end)
 function Menu:addItem(name,callback_func)
 	table.insert(self.items,{name=name,callback=callback_func})
 	return #self.items
 end
 
---- Display the menu
+-------------------------------
+-- Display the menu
 -- @usage menu:display()
+-- @return The item "ID" for an item
+-- @return The return value of the callback
 function Menu:display()
 	print(self.title)
 	print(string.rep('-',15))
